9675f484032e45982851ec2aafbe0d57b7a3bc18
sachi7038 <sachchida.nandsingh@cyfirma.com>
2023-02-01 21:40:02 +0530

    fix the normal issue

--- src/test/java/com/example/springsecurity/SpringsecurityApplicationTests.java
+++ src/test/java/com/example/springsecurity/SpringsecurityApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.springsecurity;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringsecurityApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.example.springsecurity;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class SpringsecurityApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
